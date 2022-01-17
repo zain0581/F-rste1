3eeac517fc42d6a3fb7407d9f8906603112f5e6f
Tec <Tec@SKAB1-PC-10>
2022-01-17 12:56:26 +0100

    Add project files.

---
+++ Første1/Program.cs
@@ -0,0 +1,7 @@
+﻿
+person p = new("zain","ali"), new D;
+Console.WriteLine($"{p.FirstName} {p.LastName}");
+
+
+person p2 = new();
+Console.WriteLine($"{p2.FirstName} {p2.LastName}");
---
+++ Første1/code/person.cs
@@ -0,0 +1,48 @@
+﻿global using Første1.code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Første1.code
+{
+    internal class person
+    {
+      
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public int telephonenr { get; set; }
+
+        public string BirthDateInDanishFormat{ get; set; }
+        public int Age { get; set; }
+        public double TimeSpanInTotalDays { get; set; }
+
+        public person(string firstname, string lastname,  DateTime Birthdaydate)
+
+        {
+            BirthDateInDanishFormat = Birthdaydate.ToString ("dd-mm-yyyy");
+            Age = DateTime.Now.Year- Birthdaydate.Year;
+            FirstName = firstname;
+            LastName = LastName;
+        }
+
+
+        public person()
+        {
+            BirthDateInDanishFormat = "16";
+            FirstName = "Thomas";
+            LastName = "Jensen";
+        }
+
+        public string? GetFullName()
+        {
+
+            return $"{FirstName}{LastName}";
+        }
+
+
+    }
+}
